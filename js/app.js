f6ae0c7371b55148bb07e32206ac33fccbc29514
moxilla3 <moxilla3@gmail.com.com>
2015-04-10 13:14:59 -0700

    prevent default
    Partially solved, prevent default made it appear YAY!

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
-   
-$("#submit").on("click", function() {
-  var item = $("#item").val();
-  $("#list").prepend("<li><button class='remove'>X</button>"+item+"</li>");
+
+    $('#submit').on('click', function () {
+        var item = $('#item').val();
+        $('#list').prepend('<li><button class="remove">X</button>"+item+"</li>');
+    });
+
+
+    $('#list').on('click', 'li', function () {
+        $(this).toggleClass('checked');
+    });
+
+    $('#list').on('click', '.remove', function () {
+        $(this).closest('li').remove();
+    });
+
+    $('#clear').on('click', function () {
+        $('.checked').remove();
+    });
+
+    $('#item').keydown(function (e) {
+        if (e.which == 13) {
+            var item = $('#item').val();
+            $('#list').prepend('<li><button class="remove">X</button>"+item+"</li>');
+        }
+    });
+
+
+
+
 });
-
-
-$("#list").on("click", "li", function() {
-  $(this).toggleClass("checked");
-});
-
-$("#list").on("click", ".remove", function() {
-  $(this).closest("li").remove();
-});
-
-$("#clear").on("click", function() {
-  $(".checked").remove();
-});
-
-$("#item").keydown(function(e) {
-    if (e.which == 13) {
-      var item = $("#item").val();
-  		$("#list").prepend("<li> <button class='remove'>X</button>"+item+"</li>");
-    }
-});
-
-
-
-
- });
