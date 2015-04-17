b1d7fd678d582b90f694fa97a38e807bd63c099f
moxilla3 <moxilla3@gmail.com.com>
2015-04-17 16:00:38 -0700

    solved the restore default val
    $('#item').val("");

--- js/app.js
+++ js/app.js
@@ -6,9 +6,7 @@ $(document).ready(function () {
   $("#list").prepend("<li><button class='remove'>X</button>"+item+"</li>");
 
   //trying to reset form field
-  // $('#item').each (function(){
-  //this.reset();
-  //
+   $('#item').val("");
 
     });
 
